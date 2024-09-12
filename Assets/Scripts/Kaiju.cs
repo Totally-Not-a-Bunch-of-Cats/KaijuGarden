8f82ce138a820ce53e7a3a1a94d4b5668e17efaa
superbird29 <105131626+TotallyNotaBunchofCats@users.noreply.github.com>
2024-09-12 16:43:58 -0500

    kaiju work

--- Assets/Scripts/Kaiju.cs
+++ Assets/Scripts/Kaiju.cs
@@ -4,15 +4,24 @@ using UnityEngine;
 
 public class Kaiju : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
+    
     void Update()
     {
-        
+        //Navigate to target
+    }
+
+    public void ChoosePrioTarget()
+    {
+
+    }
+
+    void ChooseTarget()
+    {
+
+    }
+
+    void Attack()
+    {
+
     }
 }
